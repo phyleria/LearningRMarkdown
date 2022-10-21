e0d3b2d2a9bc2c840446c683ec218a279ac5c4b2
Phyllis Atieno <atienophyllis032@gmail.com>
2022-10-21 15:15:37 +0300

    Updated the getSpecificTaxId function

--- R/getSpecificTaxId.R
+++ R/getSpecificTaxId.R
@@ -4,16 +4,12 @@ df <- importBugSigDB(cache = FALSE)
 
 
 getSpecificTaxId <- function(df,
-                             tax.id.type = c("metaphlan"),
-                             tax.level = "mixed",
-                             exact.tax.level = TRUE,
-                             min.size = 1) {
-  stopifnot(is.data.frame(df))
-  tax.id.type <- match.arg(tax.id.type)
-
-
-sigs<-sigs[lengths(sigs)>=min.size]
-sigs<-lapply(sigs,unique)
-return(sigs[1:3])
-}
+                             type,
+                             level
+                            ) {
+  
+  sigs <- getSignatures (tax.id.type = type,
+                         tax.level = level)
+  return(sigs)
+}getSpecificTaxId(df=df, tax.id.type = "taxname", tax.level = "species")
 
